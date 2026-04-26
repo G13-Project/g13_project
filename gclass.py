1c7c1acd8b786304d8a9c959857b877beb6e4f5e
tomas_stockler <tomasduborjal@gmail.com>
2026-04-26 22:28:22 +0100

    Adicionar ficheiro

--- gclass.py
+++ gclass.py
@@ -5,158 +5,111 @@
 #objective: Test classes based on generic class Gclass
 
 """""
-db = 'business.db'
 
-#Uncomment to test class Person (select next 4 lines and press ctrl-1)
-from classes.person import Person
-test_class = Person
-ob = '100;Antonio Silva;1966-03-12;4000.0'
-db = 'person.db'
+# -*- coding: utf-8 -*-
+"""
+@author: António Brito / Carlos Bragança
+Ficheiro: gclass.py
+Objetivo: Classe genérica para gestão de objetos e persistência em SQLite
+"""
 
-#Uncomment to test class Cliente (select next two lines and press ctrl-1)
-# from classes.cliente import Cliente
-# test_class = Cliente
-# ob = '600;Xavier;1958-03-20;5000.0'
+import sqlite3
 
-#Uncomment to test class Product
-# from classes.product import Product
-# test_class = Product
-# ob='Product1;10.9;100'
+class Gclass:
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    att = []
+    header = ''
+    des = []
 
-#Uncomment to test class Customer_login
-# from classes.customer import Customer
-# test_class = Customer
+    def __init__(self):
+        pass
 
-#Uncomment to test class Order
-# from classes.customerorder import ustomerOrder
-# from classes.customer import Customer
-# Customer.read('data/business.db')
-# test_class = Order
+    def __str__(self):
+        """Retorna uma string com os valores dos atributos do objeto"""
+        s = ""
+        for at in self.att:
+            s += str(getattr(self, at)) + ";"
+        return s[:-1]
 
-import datetime
-
-#Reads the test_class.csv file
-test_class.read('data/' + db)
-
-op = ''
-while op != 'q':
-    print('')
-    print('Choose one letter for select the option')
-    print('---------------')
-    print('l - list')
-    print('b - beginning')
-    print('n - next')
-    print('p - previous')
-    print('e - end')
-    print('---------------')
-    print('i - insert')
-    print('m - modify')
-    print('r - remove')
-    print('---------------')
-    print('s - sort by attribute')
-    print('f - find by attribute')
-    print('---------------')
-    print('q - quit')
-    print('---------------')
-    p = test_class.current()
-    print(f'\n{p}')
-    op = input('?')
-    if op == 'b':
-        test_class.first()
-    elif op == 'n':
-        test_class.nextrec()
-    elif op == 'p':
-        test_class.previous()
-    elif op == 'e':
-        test_class.last()
-    elif op == 'i':
-        p1 = None
-        if len(test_class.lst) == 0:
-            p = eval('test_class.from_string("' + ob + '")')
-            p1 = p
-        str_list = list(p.__dict__.keys())
-        attrib = str_list[0]
-        atype = type(getattr(p, attrib))
-        print('leave blank to auto-increment')
-        id = input(f'{attrib[1:]} = ')
-        if id == "":
-            id = 0
-        else:
-            id = int(id)
-        strarg = f'test_class({id}'
-        for i in range(1, len(str_list)):
-            attrib = str_list[i]
-            atype = type(getattr(p, attrib))
-            if atype == datetime.date or atype == str:
-                value = input(f'{attrib[1:]} = ')
-                strarg += f',"{value}"'
+    @classmethod
+    def get_id(cls, id):
+        """Gera um ID automático se o fornecido for None ou vazio"""
+        if id in [None, 'None', '']:
+            if len(cls.lst) == 0:
+                return 1
             else:
-                value = atype(input(f'{attrib[1:]} = '))
-                strarg += f',{value}'
-        strarg += ')'
-        if p1 != None:
-            # test_class.lst = list()
-            test_class.remove(getattr(p, str_list[0]))
-        print(strarg)
-        pobj = eval(strarg)
-        attrib = str_list[0]
-        code = getattr(pobj, attrib)
-        obj=test_class.current(code)
-        test_class.insert(code)
+                return max(cls.lst) + 1
+        return int(id)
 
-    elif op == 'm':
-        str_list = list(p.__dict__.keys())
-        attrib = str_list[0]
-        id = input(f'Record {attrib[1:]} = ') 
-        if id != "":
-            id = int(id)
-            obj=test_class.current(id)
-            print('Leave blank or new value to modify')
-            for attrib in str_list[1:]:
-                # attrib = str_list[i]
-                value = input(f'{attrib[1:]} = ') 
-                if value != "":
-                    atype = type(getattr(p, attrib))
-                    if atype == datetime.date:
-                        setattr(obj, attrib, datetime.date.fromisoformat(value))
-                    else:
-                        setattr(obj, attrib, atype(value))
-        # id = getattr(obj, test_class.att[0][1:])
-        test_class.update(id)
-    elif op == 'r':
-        str_list = list(p.__dict__.keys())
-        attrib = str_list[0]
-        atype = type(getattr(p, attrib))
-        cod = atype(input(f'{attrib[1:]} = '))
-        if cod in test_class.lst:
-            print(test_class.obj[cod])
-            print('Confirm that you want to delete the record (y/n)?', end='')
-            if input().upper() == 'Y':
-                test_class.remove(cod)
-    elif op == 'l':
-        for code in test_class.lst:
-            print(test_class.obj[code])
-    elif op == 's':
-        # Sort products by attribute in ascending order
-        attrib = input('sort by attribute name:')
-        if '_' + attrib in list(p.__dict__.keys()):
-            reverse = False
-            if input('Reverse (False):'):
-                reverse = True
-            codep = p.id         # Keep the position
-            test_class.sort(attrib, reverse)
-            for code in test_class.lst:
-                print(test_class.obj[code])
-            test_class.current(codep)
-    elif op == 'f':
-        # Find objects with a given value in an attribute
-        attrib = input('Attribute name:')
-        if '_' + attrib in list(p.__dict__.keys()):
-            atype = type(getattr(p, attrib))
-            value = atype(input('Value:'))
-            fobjs = test_class.find(value, attrib)
-            if len(fobjs) > 0:
-                test_class.current(fobjs[0].id)
-                for obj in fobjs:
-                    print(obj)
+    @classmethod
+    def read(cls, db_path):
+        """Lê os dados da tabela correspondente na base de dados SQLite"""
+        # Limpa as listas atuais para evitar duplicados ao reler
+        cls.obj = dict()
+        cls.lst = list()
+        
+        try:
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
+            # O nome da tabela é o 'header' definido na classe filha
+            cursor.execute(f"SELECT * FROM {cls.header}")
+            rows = cursor.fetchall()
+            
+            for row in rows:
+                # Instancia um novo objeto usando os dados da linha
+                cls(*row)
+                
+            conn.close()
+        except Exception as e:
+            print(f"Erro ao ler tabela {cls.header}: {e}")
 
+    @classmethod
+    def first(cls):
+        cls.pos = 0
+
+    @classmethod
+    def last(cls):
+        cls.pos = len(cls.lst) - 1
+
+    @classmethod
+    def nextrec(cls):
+        if cls.pos < len(cls.lst) - 1:
+            cls.pos += 1
+
+    @classmethod
+    def previous(cls):
+        if cls.pos > 0:
+            cls.pos -= 1
+
+    @classmethod
+    def current(cls):
+        if len(cls.lst) > 0:
+            code = cls.lst[cls.pos]
+            return cls.obj[code]
+        return None
+
+    @classmethod
+    def remove(cls, code):
+        """Remove um objeto da memória pelo seu ID"""
+        if code in cls.obj:
+            del cls.obj[code]
+            cls.lst.remove(code)
+            cls.pos = 0
+
+    @classmethod
+    def sort(cls, attrib, reverse=False):
+        """Ordena a lista de IDs por um determinado atributo"""
+        cls.lst._sort(key=lambda x: getattr(cls.obj[x], '_' + attrib), reverse=reverse)
+        cls.pos = 0
+
+    @classmethod
+    def find(cls, value, attrib):
+        """Procura objetos que tenham um determinado valor num atributo"""
+        results = []
+        for code in cls.lst:
+            if getattr(cls.obj[code], '_' + attrib) == value:
+                results.append(cls.obj[code])
+        return results
